b1c838c4cdaa9f1a0d2cc6456be87974c1e8dbc1
EricWang <wwssllabcd@gmail.com>
2012-11-14 15:06:56 +0800

    move hello.c into hello/

--- usb_driver.c
+++ usb_driver.c
@@ -5,10 +5,10 @@
 #include <linux/init.h>
 #include <linux/usb.h>
 
+MODULE_LICENSE("GPL"); 
 
-static int skel_probe(void)
-{
-}
+static int skel_probe(struct usb_interface *interface, const struct usb_device_id *id);
+
 static int skel_disconnect(void)
 {
 }
@@ -21,9 +21,6 @@ static struct usb_device_id skel_table [] = {
      { }                    /* Terminating entry */
 };
 MODULE_DEVICE_TABLE (usb, skel_table); 
-
-
-
 
 static struct usb_driver skel_driver = {
      .name =      "skeleton",
@@ -41,18 +38,114 @@ static int __init usb_skel_init(void)
          err("usb_register failed. Error number %d", result);
      return result;
 }
+module_init (usb_skel_init);
+
 static void __exit usb_skel_exit(void)
 {
      /* deregister this driver with the USB subsystem */
      usb_deregister(&skel_driver);
 }
-
-module_init (usb_skel_init);
 module_exit (usb_skel_exit);
-MODULE_LICENSE("GPL"); 
 
 
+static int skel_probe(struct usb_interface *interface, const struct usb_device_id *id);
+{
+	struct usb_skel *dev;
+    struct usb_host_interface *iface_desc;
+    struct usb_endpoint_descriptor *endpoint;
+    size_t buffer_size;
+    int i;
+    int retval = -ENOMEM;
 
+    / * allocate memory for our device state and initialize it * /
+    dev = kzalloc(sizeof(*dev), GFP_KERNEL);
+    if (!dev)
+    {
+        err("Out of memory");
+        goto error;
+    }
 
+    kref_init(&dev->kref); 
+    sema_init(&dev->limit_sem, WRITES_IN_FLIGHT);  
+    mutex_init(&dev->io_mutex);
+    spin_lock_init(&dev->err_lock);
+    init_usb_anchor(&dev->submitted);
+    init_completion(&dev->bulk_in_completion);
+
+    dev->udev = usb_get_dev(interface_to_usbdev(interface));
+    dev->interface = interface;
+
+    / * set up the endpoint information * /
+    / * use only the first bulk-in and bulk-out endpoints * /
+    iface_desc = interface->cur_altsetting;
+    for (i = 0; i < iface_desc->desc.bNumEndpoints; ++i)
+    {
+        endpoint = &iface_desc->endpoint[i].desc;
+
+        if (!dev->bulk_in_endpointAddr
+           && usb_endpoint_is_bulk_in(endpoint))
+        {
+            / * we found a bulk in endpoint * /
+            buffer_size = le16_to_cpu(endpoint->wMaxPacketSize);
+            dev->bulk_in_size = buffer_size;
+            dev->bulk_in_endpointAddr = endpoint->bEndpointAddress;
+            dev->bulk_in_buffer = kmalloc(buffer_size, GFP_KERNEL);
+            if (!dev->bulk_in_buffer)
+            {
+                err("Could not allocate bulk_in_buffer");
+                goto error;
+            }
+
+            dev->bulk_in_urb = usb_alloc_urb(0, GFP_KERNEL);
+            if (!dev->bulk_in_urb)
+            {
+                err("Could not allocate bulk_in_urb");
+                goto error;
+            }
+        }
+
+        if (!dev->bulk_out_endpointAddr
+           && usb_endpoint_is_bulk_out(endpoint))
+        {
+            / * we found a bulk out endpoint * /
+            dev->bulk_out_endpointAddr = endpoint->bEndpointAddress;
+        }
+    }
+
+    if (!(dev->bulk_in_endpointAddr && dev->bulk_out_endpointAddr))
+    {
+        err("Could not find both bulk-in and bulk-out endpoints");
+        goto error;
+    }
+
+    / * save our data pointer in this interface device * /
+    usb_set_intfdata(interface, dev);
+
+    / * we can register the device now, as it is ready * /
+    retval = usb_register_dev(interface, &skel_class);
+    if (retval)
+    {
+        / * something prevented us from registering this driver * /
+        err("Not able to get a minor for this device.");
+        usb_set_intfdata(interface, NULL);
+        goto error;
+    }
+
+    / * let the user know what node this device is now attached to * /
+    dev_info(&interface->dev,
+             "USB Skeleton device now attached to USBSkel-%d",
+             interface->minor);
+    return 0;
+
+error:
+    if (dev)
+    {
+        / * this frees allocated memory * /
+        kref_put(&dev->kref, skel_delete);
+    }
+
+    return retval;
+
+}
 
 
